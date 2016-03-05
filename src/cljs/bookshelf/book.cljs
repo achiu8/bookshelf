dc10da4027686fe864b732caf0048145ea488935
achiu8 <achiu8@gmail.com>
2016-03-04 20:55:02 -0800

    render with details state

--- src/cljs/bookshelf/book.cljs
+++ src/cljs/bookshelf/book.cljs
@@ -6,20 +6,23 @@
 (defn book-details [book]
   (html
    [:div
-    [:h4 (:book/author book)]]))
+    [:h4 (:id book)]
+    [:p (:description book)]]))
 
 
 (defn book [{:keys [book-id books]} owner]
   (let [book (some #(when (= book-id (:book/id %)) %) books)]
     (reify
+      om/IInitState
+      (init-state [_] {:details nil})
       om/IWillMount
       (will-mount [_]
         (xhr/xhr {:method      :get
                   :url         (str "books/" book-id)
-                  :on-complete #(js/console.log %)}))
-      om/IRender
-      (render [_]
+                  :on-complete #(om/set-state! owner :details %)}))
+      om/IRenderState
+      (render-state [_ {:keys [details]}]
         (html
          [:div#book
-          [:h2 (:book/title book)]
-          (book-details book)])))))
+          [:h2 (:title details)]
+          (book-details details)])))))
