a4bb75bdbee04d3d44d715f7d1c2b49d154e1d34
achiu8 <achiu8@gmail.com>
2016-03-04 20:10:23 -0800

    render book details

--- src/cljs/bookshelf/book.cljs
+++ src/cljs/bookshelf/book.cljs
@@ -3,10 +3,23 @@
             [sablono.core :as html :refer-macros [html]]
             [bookshelf.xhr :as xhr]))
 
-(defn book [app owner]
-  (reify
-    om/IRender
-    (render [_]
-      (html
-       [:div#book
-        [:h2 (str "Hello " (:book-id app))]]))))
+(defn book-details [book]
+  (html
+   [:div
+    [:h4 (:book/author book)]]))
+
+
+(defn book [{:keys [book-id books]} owner]
+  (let [book (some #(when (= book-id (:book/id %)) %) books)]
+    (reify
+      om/IWillMount
+      (will-mount [_]
+        (xhr/xhr {:method      :get
+                  :url         (str "book/" book-id)
+                  :on-complete #(js/console.log %)}))
+      om/IRender
+      (render [_]
+        (html
+         [:div#book
+          [:h2 (:book/title book)]
+          (book-details book)])))))
