a661f6d7190b60ea0dfb452859a844430e0e5155
achiu8 <achiu8@gmail.com>
2016-03-07 17:08:34 -0800

    select result using channels

--- src/cljs/bookshelf/search.cljs
+++ src/cljs/bookshelf/search.cljs
@@ -1,5 +1,7 @@
 (ns bookshelf.search
-  (:require [om.core :as om :include-macros true]
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs.core.async :as async :refer [<! put! chan]]
+            [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]
             [bookshelf.xhr :as xhr]))
 
@@ -10,7 +12,7 @@
       (om/set-state! owner :throttled true)
       (js/setTimeout #(om/set-state! owner :throttled false) 250))))
 
-(defn submit-search [books owner]
+(defn submit-search [owner]
   (let [search-term-input (om/get-node owner "search-term")
         search-term       (.-value search-term-input)]
     (when-not (empty? search-term)
@@ -26,7 +28,7 @@
               :data        selected
               :on-complete (fn [res] (om/transact! (:books app) #(conj % res)))})))
 
-(defn search-result [result owner {:keys [app parent]}]
+(defn search-result [result owner {:keys [select-ch]}]
   (reify
     om/IInitState
     (init-state [_] {:hovered false})
@@ -35,29 +37,36 @@
       (html
        [:div.clickable
         {:style         {:font-weight (when hovered "bold")}
-         :on-click      #(select-result result app parent)
+         :on-click      #(put! select-ch result)
          :on-mouse-over #(om/set-state! owner :hovered true)
          :on-mouse-out  #(om/set-state! owner :hovered false)}
         (:title result)]))))
 
-(defn search-results [results app owner]
+(defn search-results [results select-ch]
   (html
    [:div
     (om/build-all search-result
                   results
-                  {:opts {:app app :parent owner}})]))
+                  {:opts {:select-ch select-ch}})]))
 
 (defn search [app owner]
   (reify
     om/IInitState
     (init-state [_]
-      {:results []
-       :throttled false})
+      {:results   []
+       :throttled false
+       :select-ch (chan)})
+    om/IWillMount
+    (will-mount [_]
+      (let [select-ch (om/get-state owner :select-ch)]
+        (go (while true
+              (let [result (<! select-ch)]
+                (select-result result app owner))))))
     om/IRenderState
-    (render-state [_ {:keys [results]}]
+    (render-state [_ {:keys [results select-ch]}]
       (html
        [:div
         [:input
          {:ref       "search-term"
-          :on-key-up #((throttle submit-search owner) (:books app) owner)}]
-        (search-results results app owner)]))))
+          :on-key-up #((throttle submit-search owner) owner)}]
+        (search-results results select-ch)]))))
