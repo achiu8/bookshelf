861642cd79da9060e40cf91a0ddb056681ad1a1d
achiu8 <achiu8@gmail.com>
2016-03-10 00:22:47 -0800

    handle add similar book

--- src/cljs/bookshelf/similar.cljs
+++ src/cljs/bookshelf/similar.cljs
@@ -1,5 +1,6 @@
 (ns bookshelf.similar
-  (:require [om.core :as om :include-macros true]
+  (:require [cljs.core.async :as async :refer [put!]]
+            [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]))
 
 (defn to-display [expanded similar]
@@ -7,11 +8,25 @@
     similar
     (take 5 similar)))
 
-(defn similar-book [book]
-  (html
-   [:div
-    [:div.inline.similar-title (:book/title book)]
-    [:div.inline.similar-author (:book/author book)]]))
+(defn handle-add [_ book owner select-ch]
+  (om/set-state! owner :added true)
+  (put! select-ch book))
+
+(defn similar-book [book owner]
+  (reify
+    om/IInitState
+    (init-state [_] {:added false})
+    om/IRenderState
+    (render-state [_ {:keys [added]}]
+      (let [select-ch (om/get-shared owner :select-ch)]
+        (html
+         [:div
+          [:div.inline.similar-title (:book/title book)]
+          [:div.inline.similar-author (:book/author book)]
+          [:button
+           {:disabled added
+            :on-click #(handle-add % book owner select-ch)}
+           (if added "Added" "Add")]])))))
 
 (defn similar [similar owner]
   (reify
@@ -22,7 +37,7 @@
       (html
        [:div
         [:h3 "Similar"]
-        (map similar-book (to-display expanded similar))
+        (om/build-all similar-book (to-display expanded similar))
         [:span.clickable
          {:style {:display (when expanded "none")}
           :on-click #(om/set-state! owner :expanded true)}
