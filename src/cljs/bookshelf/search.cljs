5ff6892f2d7da7b2a0cf28f14319c44542a50110
achiu8 <achiu8@gmail.com>
2016-03-18 23:23:39 -0700

    handle key select

--- src/cljs/bookshelf/search.cljs
+++ src/cljs/bookshelf/search.cljs
@@ -6,41 +6,47 @@
             [bookshelf.actions :as actions]
             [bookshelf.utils :as utils]))
 
+(defn select-up [hovered]
+  (max (dec hovered) 0))
+
+(defn select-down [hovered results]
+  (min (inc hovered) (dec (count results))))
+
+(defn handle-select [_ i owner]
+  (let [result (nth (om/get-state owner :results) i)]
+    (om/set-state! owner :results [])
+    (put! (om/get-shared owner :select-ch) result)))
+
 (defn handle-keydown [e owner]
-  (if (#{38 40} (.-keyCode e))
-    (println "hi")
-    (do ((utils/throttle actions/submit-search owner) owner)
-        (put! (om/get-state owner :input-ch) (.. e -target -value)))))
+  (let [results  (om/get-state owner :results)
+        hovered  (om/get-state owner :hovered)
+        input-ch (om/get-state owner :input-ch)]
+    (condp =  (.-keyCode e)
+      38 (om/update-state! owner :hovered (fnil select-up 0))
+      40 (om/update-state! owner :hovered (fnil #(select-down % results) 0))
+      13 (handle-select nil hovered owner)
+      (do ((utils/throttle actions/submit-search owner) owner)
+          (put! input-ch (.. e -target -value))))))
 
-(defn handle-select [_ result search select-ch]
-  (om/set-state! search :results [])
-  (put! select-ch result))
+(defn search-result [result i owner]
+  (html
+   [:div.clickable
+    {:style         {:background-color (when (= i (om/get-state owner :hovered)) "#f0f0f0")}
+     :on-click      #(handle-select % i owner)
+     :on-mouse-over #(om/set-state! owner :hovered i)}
+    (:book/title result)]))
 
-(defn search-result [result owner {:keys [search]}]
-  (reify
-    om/IInitState
-    (init-state [_] {:hovered false})
-    om/IRenderState
-    (render-state [_ {:keys [hovered]}]
-      (let [select-ch (om/get-shared owner :select-ch)]
-        (html
-         [:div.clickable
-          {:style         {:background-color (when hovered "#f0f0f0")}
-           :on-click      #(handle-select % result search select-ch)
-           :on-mouse-over #(om/set-state! owner :hovered true)
-           :on-mouse-out  #(om/set-state! owner :hovered false)}
-          (:book/title result)])))))
-
-(defn search-results [results search]
+(defn search-results [results owner]
   (html
    [:div
-    (om/build-all search-result results {:opts {:search search}})]))
+    (map-indexed #(search-result %2 %1 owner) results)]))
 
 (defn search [app owner]
   (reify
     om/IInitState
     (init-state [_]
       {:results   []
+       :hovered   0
        :throttled false
        :debounced false
        :input-ch  (chan)})
@@ -49,14 +55,14 @@
       (go (while true
             (let [input-ch (om/get-state owner :input-ch)
                   delay-ch (timeout 500)
-                  [val ch] (alts! [input-ch delay-ch])]
+                  [_ ch] (alts! [input-ch delay-ch])]
               (condp = ch
                 input-ch (om/set-state! owner :debounced true)
                 delay-ch (when (om/get-state owner :debounced)
                            (actions/submit-search owner)
                            (om/set-state! owner :debounced false)))))))
     om/IRenderState
-    (render-state [_ {:keys [results]}]
+    (render-state [_ {:keys [results hovered]}]
       (html
        [:div
         [:input
