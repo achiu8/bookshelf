718a8f988dd85cf15f9d1971e2a710d3670e42bd
achiu8 <achiu8@gmail.com>
2016-03-25 15:01:48 -0700

    render added books for author

--- src/cljs/bookshelf/author.cljs
+++ src/cljs/bookshelf/author.cljs
@@ -2,11 +2,22 @@
   (:require [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]))
 
-(defn author [{:keys [author]} owner]
+(defn book [book]
   (reify
     om/IRender
     (render [_]
       (html
-       [:div#book
-        [:h2 (:author/name author)]
-        [:p {:dangerouslySetInnerHTML {:__html (:author/about author)}}]]))))
+       [:div
+        [:div (:book/title book)]]))))
+
+(defn author [{:keys [author books]} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (let [added-books (filter #(= (:author/id author) (:book/author-id %)) books)]
+        (html
+         [:div#book
+          [:h2 (:author/name author)]
+          [:p {:dangerouslySetInnerHTML {:__html (:author/about author)}}]
+          [:h3 "Added"]
+          (om/build-all book added-books)])))))
