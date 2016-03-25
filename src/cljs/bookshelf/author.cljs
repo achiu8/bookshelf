cc638f28f83b80a1c353ad456520fb05cc074d89
achiu8 <achiu8@gmail.com>
2016-03-25 16:35:50 -0700

    conditionally display and label added / other books

--- src/cljs/bookshelf/author.cljs
+++ src/cljs/bookshelf/author.cljs
@@ -10,18 +10,29 @@
        [:div
         [:div (:book/title book)]]))))
 
+(defn added-books-container [books]
+  (when (not-empty books)
+    (html
+     [:div
+      [:h3 "Added"]
+      (om/build-all book books)])))
+
+(defn other-books-container [books any-added]
+  (html
+   [:div
+    [:h3 (if any-added "Other" "Books")]
+    (om/build-all book books)]))
+
 (defn author [{:keys [author books]} owner]
   (reify
     om/IRender
     (render [_]
-      (let [existing (map :book/id books)
+      (let [existing    (map :book/id books)
             added-books (filter #(= (:author/id author) (:book/author-id %)) books)
             other-books (remove #((set existing) (:book/id %)) (:author/books author))]
         (html
          [:div#book
           [:h2 (:author/name author)]
           [:p {:dangerouslySetInnerHTML {:__html (:author/about author)}}]
-          [:h3 "Added"]
-          (om/build-all book added-books)
-          [:h3 "Other"]
-          (om/build-all book other-books)])))))
+          (added-books-container added-books)
+          (other-books-container other-books (not-empty added-books))])))))
